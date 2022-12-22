2034066f7e4896f65f1362e88f656823a046dbe4
Chusov <chusovl2004@gmail.com>
2022-12-22 09:05:43 +0500

    first commit

---
+++ wpf1/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Семерова Дарья пр-31","О разработчике");
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            phon.Background = new SolidColorBrush(Colors.LightGreen);
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            phon.Background = new SolidColorBrush(Colors.LightPink);
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            phon.Background = new SolidColorBrush(Colors.LightYellow);
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Menu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Меню";
+        }
+
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Изменить фон";
+        }
+
+        private void MenuItem_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Посмотреть информацию о разработчике";
+        }
+
+        private void MenuItem_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Закрыть окно";
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Изменить цвет фона на зеленый";
+        }
+
+        private void Button_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Изменить цвет фона на розовый";
+        }
+
+        private void Button_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Изменить цвет фона на желтый";
+        }
+
+        private void StatusBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "Строка состояния";
+        }
+
+        private void StatusBar_MouseLeave(object sender, MouseEventArgs e)
+        {
+            strsost.Text = "";
+        }
+    }
+}
---
+++ wpf2/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.LightSkyBlue;
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.GreenYellow;
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.Yellow;
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.White;
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            artibutes.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+            artibutes.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+            artibutes.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
+        {
+            artibutes.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void MenuItem_Click_9(object sender, RoutedEventArgs e)
+        {
+            artibutes.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(null != slider && draw != null)
+            {
+                draw.Height = slider.Value;
+                draw.Width = slider.Value;
+            }
+        }
+    }
+}
